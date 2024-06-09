271821551780b0fbab76a1efdd1f537141a28330
Guilherme Macedo <guilherme01macedo@gmail.com>
2024-06-09 15:38:23 +0100

    feat: header component created

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className="header__container">
+      <h2>CHATBOT</h2>
+    </div>
+  );
+}
+
+export default Header;
