e1bb05b5eddb43937a45a8285c68fa7a12323896
Guilherme Macedo <guilherme01macedo@gmail.com>
2024-06-15 19:35:34 +0100

    feat: image added on the header and title changed to MAIA

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
+import headerImg from './header_image.jpg';
+import linkedinImg from './linkedin.png';
 
 const Header = () => {
+  const [displayText, setDisplayText] = useState('MAIA');
+
+  const handleMouseEnter = () => {
+    setDisplayText('MY AI ASSISTANT');
+  };
+
+  const handleMouseLeave = () => {
+    setDisplayText('MAIA');
+  };
+
   return (
     <div className="header__container">
-      <h1>MY AI ASSISTANT</h1>
+        <div class="header__image-container">
+          <img src={headerImg} alt="Header profile pic" />
+          <img className='header__linkedin-image-container' src={linkedinImg} alt="Linkedin logo" />
+        </div>
+      <h1
+        className="header__animated-text"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        {displayText}
+      </h1>
     </div>
   );
 }
