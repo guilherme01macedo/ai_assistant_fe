98328f4df504002006ec7d0b0ba89d589e0587aa
Guilherme Macedo <guilherme01macedo@gmail.com>
2024-06-17 20:47:12 +0100

    feat: last searched string added to prevent the same question to be randomized twice in a row

--- src/hooks/useStringRandomizer.js
+++ src/hooks/useStringRandomizer.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { trackEvent } from "../utils/analytics";
 import { POSSIBLE_RANDOM_QUESTIONS } from "../utils/constants";
 
 const useStringRandomizer = (setStringValue, getSearchResult) => {
-    trackEvent('User Interaction', 'Clicked Button', 'String randomizer');
+    const [lastSearched, setLastSearched] = useState('');
+
     const generateRandomString = () => {
-        const randomString = POSSIBLE_RANDOM_QUESTIONS[Math.floor(Math.random()*POSSIBLE_RANDOM_QUESTIONS.length)];
+        const filteredPossibleQuestions = POSSIBLE_RANDOM_QUESTIONS.filter(a=>a!==lastSearched)
+        trackEvent('User Interaction', 'Clicked Button', 'String randomizer');
+        const randomString = filteredPossibleQuestions[Math.floor(Math.random()*filteredPossibleQuestions.length)];
         setStringValue(randomString);
-        getSearchResult(randomString)
+        getSearchResult(randomString);
+        setLastSearched(randomString);
       };
     
   return { generateRandomString};
